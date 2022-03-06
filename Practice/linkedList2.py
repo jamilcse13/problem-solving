e15b7a0ea3de72b40d44315fc701e854854e3d17
jamil <jamil.cse13@gmail.com>
2022-03-06 08:10:32 +0600

    added a node after any given node

--- Practice/linkedList2.py
+++ Practice/linkedList2.py
@@ -1,3 +1,4 @@
+# create Node
 class Node:
     def __init__(self, data = None, next = None):
         self.data = data
@@ -9,9 +10,17 @@ class LinkedList:
         self.head = None
         self.prev = None
     
+    # create a linkedList
     def insert(self, data):
+        # 1. Create a new node
+        # 2. Put in the data
+        # 3. Set next as None
         new_node = Node(data)
-
+        
+        # 4. If the Linked List is empty, then make the
+        #    new node as head
+        # 5. Else traverse till the last node
+        # 6. Change the next of last node
         if self.head == None:
             self.head = new_node
             self.prev = new_node
@@ -19,12 +28,37 @@ class LinkedList:
             self.prev.next = new_node
             self.prev = self.prev.next
     
+
+    # Function to insert a new node at the beginning
     def push(self, new_data):
+        # 1-2. Allocate the Node &
+        # Put in the data
         new_node = Node(new_data)
 
+        # 3. Make next of new Node as head
         new_node.next = self.head
+        # 4. Move the head to point to new Node
         self.head = new_node
+
+
+    # Inserts a new node after the given prev_node
+    def insertAfter(self, prev_node, new_data):
+        #1. check if the given prev_node exists
+        if prev_node is None:
+            print("The given previous data must be in linkedList")
+            return
+        
+        # 2. Create new node &
+        # 3. Put in the data
+        new_node = Node(new_data)
+
+        # 4. Make next of new Node as next of prev_node
+        new_node.next = prev_node.next
+        # 5. make next of prev_node as new_node
+        prev_node.next = new_node
     
+    
+    # print the linked list
     def printLinkedList(self):
         current = self.head
         while(current):
