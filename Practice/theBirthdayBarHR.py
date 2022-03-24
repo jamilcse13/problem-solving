588ab08e716f43eef5418240f332895d076909d0
Jamil Ahsan <jamil.cse13@gmail.com>
2022-03-24 12:51:32 +0600

    modified birthday bar code

--- Practice/theBirthdayBarHR.py
+++ Practice/theBirthdayBarHR.py
@@ -1,30 +1,32 @@
-import math
+def theBirthdayBar():
+    n = int(input())
 
-n = int(input())
+    s = list(map(int, input().split()))[:n]
+    nums = list(map(int, input().split()))
+    d = nums[0]
+    m = nums[1]
 
-s = list(map(int, input().split()))[:n]
-nums = list(map(int, input().split()))
-d = nums[0]
-m = nums[1]
+    length = len(s)
+    ans = 0
+    val = 0
 
-length = len(s)
-ans = 0
-val = 0
-
-if length == 1:
-    if s[0] == d:
-        print(1)
+    if length == 1:
+        if s[0] == d:
+            return 1
+        else:
+            return 0
     else:
-        print(0)
-        raise SystemExit
-    
-for i in range(0, m):
-    val += s[i]
-if val == d:
-    ans += 1
+        for i in range(0, m):
+            val += s[i]
+        if val == d:
+            ans += 1
 
-for i in range(1, length-m+1):
-    val = val - s[i-1] + s[i+m-1]
-    if val == d:
-        ans += 1
-print(ans)
+        for i in range(1, length - m + 1):
+            val = val - s[i - 1] + s[i + m - 1]
+            if val == d:
+                ans += 1
+        return ans
+
+
+if __name__ == "__main__":
+    print(theBirthdayBar())
