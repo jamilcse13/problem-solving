09b62ddf315c979b0a87eaa62799268b183e8da1
jamil <jamil.cse13@gmail.com>
2022-03-30 00:14:24 +0600

    solved codechef pawri meme  problem

---
+++ CodeChef/Pawri Meme.py
@@ -0,0 +1,11 @@
+def pawriMeme():
+    for _ in range(int(input())):
+        s = input()
+        ans = s.replace('party', 'pawri')
+        print(ans)
+
+if __name__ == "__main__":
+    pawriMeme()
+
+# Time Complexity: O(1)
+# Space Complexity: O(1)
